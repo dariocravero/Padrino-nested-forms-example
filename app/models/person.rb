091f8d66cdcb3baa4734d36d4a07ca409826383b
Darío Javier Cravero <dario@q-innova.com.ar>
2011-04-05 10:19:00 -0300

    Addded :reject_if => :all_blank but doesn't work as expected

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ActiveRecord::Base
   has_many :addresses, :class_name => "Address"
-  accepts_nested_attributes_for :addresses, :allow_destroy => true
+  accepts_nested_attributes_for :addresses, :allow_destroy => true, :reject_if => :all_blank
 end
